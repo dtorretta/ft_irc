66ac9b620d17fb7af7f83f5b6598f0d7b89669a6
Miguel Andrade <miguandr@student.42.fr>
2025-08-19 11:53:48 +0200

    update in ChannelCommands.hpp

--- includes/commands/ChannelCommands.hpp
+++ includes/commands/ChannelCommands.hpp
@@ -15,18 +15,19 @@ class Server;
 
 // Command function pointer type
 //typedef void (Server::*CommandHandler)(std::string cmd, int fd);
+//si no cambia el Server::Parser, esta funcion se puede eliminar
 
 // This macro defines all Server command methods
-// Include this inside the Server class definition
 #define SERVER_COMMAND_METHODS \
-	/* JOIN Command */ \
+	/***JOIN Command***/ \
 	void	JOIN(std::string cmd, int fd); \
 	std::vector<std::pair<std::string, std::string> > SplitJOIN(std::string cmd); \
 	void	Channel_Exist(Channel *channel, Client *client, int fd, std::string key, std::string name); \
 	void	Channel_Not_Exist(std::string channel_name, Client *client, int fd); \
-	/* PART Command */ \
+	/***PART Command***/ \
 	void	PART(std::string cmd, int fd); \
 	std::vector<std::string> SplitPART(std::string cmd); \
-	/* PRIVMSG Command */ \
+	/***PRIVMSG Command***/ \
 	void	PRIVMSG(std::string cmd, int fd); \
 	std::vector<std::string> SplitPM(std::string command);
+	/***TOPIC Command***/ \
